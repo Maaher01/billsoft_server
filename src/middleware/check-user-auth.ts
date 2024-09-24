0e030469e8cf41fd49033311363272dc59edebd3
unknown <maaher1145@gmail.com>
2024-09-25 01:09:59 +0600

    Added user authentication check

--- src/middleware/check-user-auth.ts
+++ src/middleware/check-user-auth.ts
@@ -1,30 +1,59 @@
-import jwt from "jsonwebtoken";
-import { Request, Response } from "express";
+import jwt, { JwtPayload } from "jsonwebtoken";
+import { Request, Response, NextFunction } from "express";
 
-module.exports = (req: Request, res: Response) => {
-  const authHeader = req.get("Authorization");
-  if (!authHeader) {
-    const error = new Error("Sorry! Not Authorized");
-    error.statusCode = 401;
-    throw error;
-  }
-  const token = authHeader.split(" ")[1];
-  let decodedToken;
+interface CustomRequest extends Request {
+	userData?: {
+		fullname: string;
+		userId: string;
+	};
+}
 
-  try {
-    decodedToken = jwt.verify(token, process.env.TOKEN_SECRET as string);
-    req.userData = {
-      fullname: decodedToken.UserInfo.fullname,
-      userId: decodedToken.UserInfo.userId,
-    };
-    next();
-  } catch (err) {
-    err.statusCode = 500;
-    throw err;
-  }
-  if (!decodedToken) {
-    const error = new Error("Authorization Error!");
-    error.statusCode = 401;
-    throw error;
-  }
+interface DecodedToken extends JwtPayload {
+	UserInfo: {
+		fullname: string;
+		userId: string;
+	};
+}
+
+module.exports = (req: CustomRequest, res: Response, next: NextFunction) => {
+	const authHeader = req.get("Authorization");
+
+	if (!authHeader) {
+		return res.status(401).json({
+			message: "Sorry! You are not authorized",
+		});
+	}
+
+	const token = authHeader.split(" ")[1];
+
+	if (!token) {
+		return res.status(401).json({
+			message: "Token not found in authorization header",
+		});
+	}
+
+	try {
+		const decodedToken = jwt.verify(
+			token,
+			process.env.TOKEN_SECRET as string
+		) as DecodedToken;
+
+		if (!decodedToken) {
+			return res.status(401).json({
+				message: "Authorization error",
+			});
+		}
+
+		req.userData = {
+			fullname: decodedToken.UserInfo.fullname,
+			userId: decodedToken.UserInfo.userId,
+		};
+
+		next();
+	} catch (err) {
+		return res.status(500).json({
+			message: "Authorization error",
+			error: err,
+		});
+	}
 };
